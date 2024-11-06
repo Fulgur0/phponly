ac4ef07caf52e069ca611247d1973dcff23ee336
Fulgur <fulgurgithub@gmail.com>
2024-11-06 12:11:26 +0100

    Removed config reference from example controller

--- controller/Home.php
+++ controller/Home.php
@@ -3,14 +3,12 @@
 namespace controller;
 
 use model\UserModel;
-use sys\core\utils\Config;
 use sys\core\Controller;
 
 class Home extends Controller
 {
     public function index()
     {
-        $config = new Config();
         $user = new UserModel();
         echo 'Hello world ' . json_encode($user->get(1)[0]['username']);
     }
